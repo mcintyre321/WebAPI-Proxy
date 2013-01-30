8ea038bb1a3c40c1152bb7a699ac128fe3eda3b1
Harry McIntyre <mcintyre321@gmail.com>
2013-01-30 17:40:52 +0000

    Remove the extra stuff

--- WebApiProxy/ForwardProxyMessageHandler.cs
+++ WebApiProxy/ForwardProxyMessageHandler.cs
@@ -9,14 +9,12 @@ namespace WebApiProxy
     {
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            //var host = "localhost:3000";
-            //request.Headers.Host = host;
             request.Headers.Add("X-Forwarded-For", request.GetClientIp());
             if (request.Method == HttpMethod.Get || request.Method == HttpMethod.Trace) request.Content = null;
+
+            request.RequestUri = new Uri(request.RequestUri.ToString().Replace(":3002", "")); //comes through with the port for the proxy, rewrite to port 80
             
-//            request.RequestUri = request.RequestUri;
-            request.RequestUri = new Uri(request.RequestUri.ToString().Replace(":3002", ""));
-            //request.RequestUri = new Uri("http://" + host + request.RequestUri.PathAndQuery);
+            
             request.Headers.AcceptEncoding.Clear();
             return new HttpClient().SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
                                    .ContinueWith(r =>
--- WebApiProxy/Program.cs
+++ WebApiProxy/Program.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.ServiceModel;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Filters;
 using System.Web.Http.SelfHost;
-using WebApiProxy.Authorization;
 
 namespace WebApiProxy
 {
@@ -25,25 +16,18 @@ namespace WebApiProxy
             {
                 var config = new HttpSelfHostConfiguration("http://localhost:3002/")
                 {
-                    MaxReceivedMessageSize = 1024 * 1024 * 1024,
+                    MaxReceivedMessageSize = 1024*1024*1024,
                     TransferMode = TransferMode.Streamed,
                     MessageHandlers =
-                        {
-                            //new ValidateTokenHandler(),
-                            //new IPHostValidationHandler()
-                        }
+                    {
+                        new ForwardProxyMessageHandler()
+                    }
                 };
-                config.MessageHandlers.Clear();
-                config.MessageHandlers.Add(new AddCommentHandler());
-                config.MessageHandlers.Add(new ForwardProxyMessageHandler());
-
-                config.Routes.MapHttpRoute("Proxy", "{*path}", new {controller = "Proxy", path = ""});
 
                 server = new HttpSelfHostServer(config);
 
                 server.OpenAsync().Wait();
 
-                var timer = new Timer(Report, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1));
                 Console.WriteLine("Hit ENTER to exit");
                 Console.ReadLine();
             }
@@ -55,15 +39,5 @@ namespace WebApiProxy
                 }
             }
         }
-
-        private static void Report(object state)
-        {
-            var pos = new {Console.CursorLeft, Console.CursorTop};
-            Console.SetCursorPosition(0,0);
-            Console.Write(string.Format("{0:n0}", Process.GetCurrentProcess().PagedMemorySize64 / 1024) + "kb            ");
-            Console.SetCursorPosition(pos.CursorLeft, pos.CursorTop);
-        }
     }
-    
- 
 }
