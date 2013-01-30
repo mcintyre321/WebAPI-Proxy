513a0c590fe6bb4c2f54cb1baa4e6be7bd103ed2
Harry McIntyre <mcintyre321@gmail.com>
2013-01-30 17:51:18 +0000

    Use async keyword

--- WebApiProxy/ForwardProxyMessageHandler.cs
+++ WebApiProxy/ForwardProxyMessageHandler.cs
@@ -7,23 +7,16 @@ namespace WebApiProxy
 {
     public class ForwardProxyMessageHandler : DelegatingHandler
     {
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             request.Headers.Add("X-Forwarded-For", request.GetClientIp());
             if (request.Method == HttpMethod.Get || request.Method == HttpMethod.Trace) request.Content = null;
-
             request.RequestUri = new Uri(request.RequestUri.ToString().Replace(":3002", "")); //comes through with the port for the proxy, rewrite to port 80
-            
-            
             request.Headers.AcceptEncoding.Clear();
-            return new HttpClient().SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
-                                   .ContinueWith(r =>
-                                   {
-                                       r.Result.Headers.TransferEncodingChunked = null; //throws an error on calls to WebApi results
-
-                                       if (request.Method == HttpMethod.Head) r.Result.Content = null;
-                                       return r.Result;
-                                   });
+            var responseMessage = await new HttpClient().SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            responseMessage.Headers.TransferEncodingChunked = null; //throws an error on calls to WebApi results
+            if (request.Method == HttpMethod.Head) responseMessage.Content = null;
+            return responseMessage;
         }
 
     }
